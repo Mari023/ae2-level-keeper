3ad86a0f4efbf963339913bf9bd34b7dcc4d8fa0
Mari023 <38946771+Mari023@users.noreply.github.com>
2021-08-27 00:07:06 +0200

    somewhat fix blockentity

--- common/src/main/java/net/examplemod/LevelKeeperBlockEntity.java
+++ common/src/main/java/net/examplemod/LevelKeeperBlockEntity.java
@@ -1,22 +1,35 @@
 package net.examplemod;
 
-import alexiil.mc.lib.attributes.item.FixedItemInv;
+import appeng.api.networking.IGridNode;
+import appeng.api.networking.security.IActionHost;
 import appeng.api.networking.security.IActionSource;
 import appeng.api.util.AECableType;
 import appeng.api.util.AEPartLocation;
 import appeng.api.util.DimensionalCoord;
+import appeng.me.helpers.AENetworkProxy;
+import appeng.me.helpers.IGridProxyable;
 import appeng.me.helpers.MachineSource;
-import appeng.tile.grid.AENetworkInvTileEntity;
-import appeng.util.inv.InvOperation;
+import appeng.tile.AEBaseTileEntity;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
-public class LevelKeeperBlockEntity extends AENetworkInvTileEntity {
+import java.util.EnumSet;
+
+public class LevelKeeperBlockEntity extends AEBaseTileEntity implements IActionHost, IGridProxyable {
     private final IActionSource mySrc;
+    private final AENetworkProxy proxy;
 
     public LevelKeeperBlockEntity(BlockEntityType<?> tileEntityTypeIn) {
         super(tileEntityTypeIn);
         mySrc = new MachineSource(this);
+        proxy = new AENetworkProxy(this, "level_keeper", new ItemStack(ExampleMod.LEVEL_KEEPER_DEFINITION.blockItem()), true);
+        proxy.setValidSides(EnumSet.allOf(Direction.class));
+    }
+
+    @Override
+    public AENetworkProxy getProxy() {
+        return proxy;
     }
 
     @Override
@@ -25,17 +38,23 @@ public class LevelKeeperBlockEntity extends AENetworkInvTileEntity {
     }
 
     @Override
+    public void gridChanged() {
+
+    }
+
+    @Override
+    public IGridNode getGridNode(AEPartLocation aePartLocation) {
+        return proxy.getNode();
+    }
+
+    @Override
     public AECableType getCableConnectionType(AEPartLocation aePartLocation) {
         return AECableType.SMART;
     }
 
-    @Override
-    public FixedItemInv getInternalInventory() {
-        return null;
-    }
 
     @Override
-    public void onChangeInventory(FixedItemInv fixedItemInv, int i, InvOperation invOperation, ItemStack itemStack, ItemStack itemStack1) {
-
+    public IGridNode getActionableNode() {
+        return proxy.getNode();
     }
 }
